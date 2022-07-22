d57d32ca925d7544e14d026fdcf7833407c41f9c
Deepak <deepakkumar750206@gmail.com>
2022-07-22 17:20:22 +0530

    replaced useState with useReducer

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,15 +1,39 @@
 /* eslint-disable */
 
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useReducer } from 'react';
 import { useParams } from 'react-router-dom';
 import { apiGet } from '../misc/config';
+
+
+
+const reducer =(prevState, action) =>{
+switch(action.type){
+
+  case 'FETCH_SUCCESS': {
+    return { isLoading: false, error: null, show: action.show}
+  }
+
+  case 'FETCH_FAILED': {
+    return {...prevState, isLoading: false, error: action.error}
+  }
+
+  default:
+     return prevState;
+}
+}
+
+const initialState = {
+  show: null,
+  isLoading: true,
+  error: null
+}
 
 function Show() {
   const { id } = useParams();
 
-  const [show, setShow] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
- const [error, setError] = useState(null);
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  console.log('state', state)
 
   useEffect( ()=>{
 
@@ -18,12 +42,14 @@ function Show() {
     apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`)
     .then(results =>{  
         if(isMounted){
-          setShow(results);
-          setIsLoading(false);
+
+          dispatch( { type: 'FETCH_SUCCESS', show: results})
         }
        })
     .catch(err =>{
       if(isMounted){
+
+        dispatch( { type: 'FETCH_FAILED', error: err.message})
         setError(err.message);
         setIsLoading(false);
       }
@@ -34,14 +60,15 @@ function Show() {
 
   }, [id]);
 
-  console.log('show', show);
+//   console.log('show', show);
+//   console.log('isLoading', isLoading);
 
-if(isLoading){
-  return <div>Data is being loaded</div>
-}
-if(error){
-  return <div>Error message occured: {Error}</div>
-}
+// if(isLoading){
+//   return <div>Data is being loaded</div>
+// }
+// if(error){
+//   return <div>Error message occured: {Error}</div>
+// }
 
   return (
     <div>
