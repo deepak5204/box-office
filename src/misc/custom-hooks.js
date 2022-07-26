91bcd8bb3db9743f87fd04b6e81543502a9f634b
Deepak <deepakkumar750206@gmail.com>
2022-07-26 15:07:58 +0530

    finished starred page

--- src/misc/custom-hooks.js
+++ src/misc/custom-hooks.js
@@ -2,29 +2,24 @@
 
 import { useReducer,useEffect } from 'react';
 
-
 function showsReducer(prevState, action){
 switch(action.type){
-    
     case 'ADD': {
-        return prevState.filter(showId => showId !== action.showId)
+        return [...prevState, action.showId];
     }
 
     case 'REMOVE': {
-        return prevState.filter(showId => showId !== action.showId);
+        return prevState.filter(showId => showId !== action.showId)
     }
-
     default: 
-    return prevState;
-        
+    return prevState;        
 }
-
 }
 
 
 
 function usePersistedReducer(reducer, initialState, key){
-    const [state, dispatch] = useReducer(reducer, initialState, initial => () =>{
+    const [state, dispatch] = useReducer(reducer, initialState, initial =>{
         const persisted = localStorage.getItem(key);
 
         return persisted ? JSON.parse(persisted) : initial;
@@ -32,15 +27,12 @@ function usePersistedReducer(reducer, initialState, key){
 
 
     useEffect( ()=> {
-        localStorage.setItem(key, JSON.stringify(state))
-    }, [state, key])
+        localStorage.setItem(key, JSON.stringify(state));
+    }, [state, key]);
 
     return [state, dispatch];
-
 }
 
-export function useShows(key= "shows"){
-
-
+export function useShows(key= 'shows'){
     return usePersistedReducer(showsReducer, [], key);
 }
