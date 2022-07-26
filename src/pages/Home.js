037dbe9f6a97b932b4a28a883dc15aabff1571c3
Deepak <deepakkumar750206@gmail.com>
2022-07-26 21:16:35 +0530

    styled the entire app

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,6 +5,8 @@ import {apiGet} from '../misc/config'
 import ShowGrid from '../components/show/ShowGrid';
 import ActorGrid from '../components/actor/ActorGrid';
 import { useLastQuery } from '../misc/custom-hooks';
+import { RadioInputsWrapper, SearchButtonWrapper, SearchInput } from './Home.styled';
+import CustomRadio from '../components/CustomRadio';
 
 function Home()  {
   const [input, setInput] = useLastQuery();
@@ -49,42 +51,46 @@ const onKeyDown = ev =>{
 
   return (
    <MainPageLayout>
-    <input 
+    <SearchInput 
       type="text" 
-      placeholder='Search for something'
+      placeholder='wow'
       onChange = { onInputChange } 
       onKeyDown={onKeyDown} 
       value= {input}
     />
 
-    <div>
-      <label htmlFor='shows-search'>
-        Shows
-        <input 
+    <RadioInputsWrapper>
+      <div>
+        <CustomRadio 
+        label="Shows"
           id='shows-search' 
-          type="radio" 
           value="shows" 
           checked={isShowsSearch}
           onChange={onRadioChange} 
-        />
-      </label>
+        />    
+      </div>
 
-      <label htmlFor='actors-search'>
-        Actors
-        <input 
+
+      <div>
+        <CustomRadio 
+          label="Actors"
           id='actors-search' 
-          type="radio" 
           value="people"
           checked={!isShowsSearch} 
           onChange={onRadioChange} 
-        />
-      </label>
-    </div>
+        />    
+      </div>
+
+
+    </RadioInputsWrapper>
+
+      <SearchButtonWrapper>
 
       <button type="button" 
         onClick={onSearch}>
           Search
       </button>
+          </SearchButtonWrapper>
       {renderResults()}
    </MainPageLayout>
   )
